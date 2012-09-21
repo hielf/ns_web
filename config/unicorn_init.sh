7aebc564b0e61cc136afa8bf1dac9ee3334ad0b3
hielf <wercub@tom.com>
2012-09-22 03:24:21 +0800

    sth

--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -5,70 +5,27 @@ set -e
 
 # Feel free to change any of the following variables for your app:
 TIMEOUT=${TIMEOUT-60}
-APP_ROOT=/home/ns_web
+UNICORN=$rvm_path/bin/bootup_unicorn
+CONFIG_FILE=$APP_ROOT/config/unicorn.rb
+APP_HOME=/home/ns_web
 PID=$APP_ROOT/tmp/pids/unicorn.pid
 CMD="$APP_ROOT/bin/unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
 # INIT_CONF=$APP_ROOT/config/init.conf
 action="$1"
 set -u
 
-# test -f "$INIT_CONF" && . $INIT_CONF
-
-old_pid="$PID.oldbin"
-
-cd $APP_ROOT || exit 1
-
-sig () {
-	test -s "$PID" && kill -$1 `cat $PID`
-}
-
-oldsig () {
-	test -s $old_pid && kill -$1 `cat $old_pid`
-}
-
-case $action in
-start)
-	sig 0 && echo >&2 "Already running" && exit 0
-	$CMD
-	;;
-stop)
-	sig QUIT && exit 0
-	echo >&2 "Not running"
-	;;
-force-stop)
-	sig TERM && exit 0
-	echo >&2 "Not running"
-	;;
-restart|reload)
-	sig HUP && echo reloaded OK && exit 0
-	echo >&2 "Couldn't reload, starting '$CMD' instead"
-	$CMD
-	;;
-upgrade)
-	if sig USR2 && sleep 2 && sig 0 && oldsig QUIT
-	then
-		n=$TIMEOUT
-		while test -s $old_pid && test $n -ge 0
-		do
-			printf '.' && sleep 1 && n=$(( $n - 1 ))
-		done
-		echo
-
-		if test $n -lt 0 && test -s $old_pid
-		then
-			echo >&2 "$old_pid still exists after $TIMEOUT seconds"
-			exit 1
-		fi
-		exit 0
-	fi
-	echo >&2 "Couldn't upgrade, starting '$CMD' instead"
-	$CMD
-	;;
-reopen-logs)
-	sig USR1
-	;;
-*)
-	echo >&2 "Usage: $0 <start|stop|restart|upgrade|force-stop|reopen-logs>"
-	exit 1
-	;;
+case "$1" in
+  start)
+  $UNICORN -c $CONFIG_FILE -E production -D
+  ;;
+  stop)
+  kill -QUIT `cat $APP_ROOT/tmp/unicorn.pid`
+  ;;
+  restart|force-reload)
+    kill -USR2 `cat $APP_ROOT/tmp/unicorn.pid`
+  ;;
+  *)
+   echo "Usage: $SCRIPTNAME {start|stop|restart|force-reload}" >&2
+   exit 3
+   ;;
 esac
