7e566f37c808b5d151750892e45aa78eb690808a
hielf <wercub@tom.com>
2012-09-23 23:01:32 +0800

    commit

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,38 +1,9 @@
-# -*- encoding: utf-8 -*-
+root = "/home/webmaster/ns_web"
+working_directory root
+pid "#{root}/tmp/pids/unicorn.pid"
+stderr_path "#{root}/log/unicorn.log"
+stdout_path "#{root}/log/unicorn.log"
 
-root_path = File.expand_path '../', File.dirname(__FILE__)
-# 日志
-log_file = root_path + '/log/unicorn.log'
-err_log  = root_path + '/log/unicorn_error.log'
-# 进程标识
-pid_file = '/tmp/unicorn_padrino.pid'
-old_pid = pid_file + '.oldbin'
-# 通道
-socket_file = '/tmp/unicorn_padrino.sock'
-
-worker_processes 6
-working_directory root_path
+listen "/tmp/unicorn.blog.sock"
+worker_processes 2
 timeout 30
-# 侦听
-listen 8080, tcp_nopush: false
-listen socket_file, backlog: 1024
-
-pid pid_file
-stderr_path err_log
-stdout_path log_file
-
-preload_app true
-
-before_exec do |server|
-  ENV['BUNDLE_GEMFILE'] = root_path + '/Gemfile'
-end
-
-before_fork do |server, worker|
-  if File.exists?(old_pid) && server.pid != old_pid
-    begin
-      Process.kill('QUIT', File.read(old_pid).to_i)
-    rescue Errno::ENOENT, Errno::ESRCH
-      puts "Send 'QUIT' signal to unicorn error!"
-    end
-  end
-end
--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -1,30 +1,84 @@
 #!/bin/sh
+### BEGIN INIT INFO
+# Provides:          unicorn
+# Required-Start:    $remote_fs $syslog
+# Required-Stop:     $remote_fs $syslog
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Manage unicorn server
+# Description:       Start, stop, restart unicorn server for a specific application.
+### END INIT INFO
 set -e
-# Example init script, this can be used with nginx, too,
-# since nginx and unicorn accept the same signals
 
 # Feel free to change any of the following variables for your app:
 TIMEOUT=${TIMEOUT-60}
-APP_ROOT=/home/ns_web
-UNICORN=$rvm_path/bin/bootup_unicorn
-CONFIG_FILE=$APP_ROOT/config/unicorn.rb
-CMD="$APP_ROOT/bin/unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
-
-action="$1"
+APP_ROOT=/home/webmaster/ns_web
+PID=$APP_ROOT/tmp/pids/unicorn.pid
+CMD="cd $APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
+AS_USER=deployer
 set -u
 
+OLD_PIN="$PID.oldbin"
+
+sig () {
+  test -s "$PID" && kill -$1 `cat $PID`
+}
+
+oldsig () {
+  test -s $OLD_PIN && kill -$1 `cat $OLD_PIN`
+}
+
+run () {
+  if [ "$(id -un)" = "$AS_USER" ]; then
+    eval $1
+  else
+    su -c "$1" - $AS_USER
+  fi
+}
+
 case "$1" in
-  start)
-  $UNICORN -c $CONFIG_FILE -E production -D
+start)
+  sig 0 && echo >&2 "Already running" && exit 0
+  run "$CMD"
   ;;
-  stop)
-  kill -QUIT `cat $APP_ROOT/tmp/unicorn.pid`
+stop)
+  sig QUIT && exit 0
+  echo >&2 "Not running"
   ;;
-  restart|force-reload)
-    kill -USR2 `cat $APP_ROOT/tmp/unicorn.pid`
+force-stop)
+  sig TERM && exit 0
+  echo >&2 "Not running"
   ;;
-  *)
-   echo "Usage: $SCRIPTNAME {start|stop|restart|force-reload}" >&2
-   exit 3
-   ;;
+restart|reload)
+  sig HUP && echo reloaded OK && exit 0
+  echo >&2 "Couldn't reload, starting '$CMD' instead"
+  run "$CMD"
+  ;;
+upgrade)
+  if sig USR2 && sleep 2 && sig 0 && oldsig QUIT
+  then
+    n=$TIMEOUT
+    while test -s $OLD_PIN && test $n -ge 0
+    do
+      printf '.' && sleep 1 && n=$(( $n - 1 ))
+    done
+    echo
+
+    if test $n -lt 0 && test -s $OLD_PIN
+    then
+      echo >&2 "$OLD_PIN still exists after $TIMEOUT seconds"
+      exit 1
+    fi
+    exit 0
+  fi
+  echo >&2 "Couldn't upgrade, starting '$CMD' instead"
+  run "$CMD"
+  ;;
+reopen-logs)
+  sig USR1
+  ;;
+*)
+  echo >&2 "Usage: $0 <start|stop|restart|upgrade|force-stop|reopen-logs>"
+  exit 1
+  ;;
 esac
